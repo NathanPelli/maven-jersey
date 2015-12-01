b6dd94cfbb76908f2306973b8db9cafec71511f8
NathanPelli <alanjohnsongates@gmail.com>
2015-12-01 18:30:32 +0800

    1

---
+++ src/main/java/andy/maven_jersey/App.java
@@ -0,0 +1,13 @@
+package andy.maven_jersey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
